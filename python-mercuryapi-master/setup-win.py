b3145d8f65e7c91d1f4af350be2038b7fccd8fe8
thomaskaufi <thomas@kaufmanas.dk>
2026-08-07 12:11:33 +0200

    Upload Python Mercury

---
+++ python-mercuryapi-master/setup-win.py
@@ -0,0 +1,54 @@
+# python setup-win.py bdist_wininst -p win32
+# python setup-win.py bdist_wininst -p amd64
+
+from glob import glob;
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+# you need to download https://www.jadaktech.com/documentation/rfid/mercuryapi
+# https://www.jadaktech.com/wp-content/uploads/2020/01/mercuryapi-YEATS-1.31.4.35-1.zip
+# FIXME: make sure the patch matches the mercuryapi version you have
+mercuryapi = "mercuryapi-1.31.4.35/c/src/"
+
+setup(name="mercuryapi", version="0.5.4",
+      ext_modules=[Extension("mercury",
+                             [mercuryapi + "api/hex_bytes.c",
+                              mercuryapi + "api/llrp_reader.c",
+                              mercuryapi + "api/llrp_reader_l3.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_array.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_connection.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_element.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_encdec.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_error.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_framedecode.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_frameencode.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_frameextract.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_genout.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_hdrfd.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_typeregistry.c",
+                              mercuryapi + "api/ltkc_win32/src/ltkc_xmltextencode.c",
+                              mercuryapi + "api/ltkc_win32/src/tm_ltkc_genout.c",
+                              mercuryapi + "api/osdep_win32.c",
+                              mercuryapi + "api/serial_reader.c",
+                              mercuryapi + "api/serial_reader_l3.c",
+                              mercuryapi + "api/serial_transport_tcp_win32.c",
+                              mercuryapi + "api/serial_transport_win32.c",
+                              mercuryapi + "api/tmr_loadsave_configuration.c",
+                              mercuryapi + "api/tmr_param.c",
+                              mercuryapi + "api/tmr_strerror.c",
+                              mercuryapi + "api/tmr_utils.c",
+                              mercuryapi + "api/tm_reader.c",
+                              mercuryapi + "api/tm_reader_async.c",
+                              "mercury.c"],
+                             define_macros=[('WIN32', '1'),
+                                            ('HAVE_STRUCT_TIMESPEC', None),
+                                            ('_CRT_SECURE_NO_WARNINGS', None)],
+                             include_dirs=[mercuryapi + 'api',
+                                           mercuryapi + 'api/ltkc_win32/inc',
+                                           mercuryapi + 'pthreads-win32/pthreads.2'],
+                             libraries=["pthreadVC2"],
+                             # FIXME: update to include the path to pthreadVC2.lib
+                             library_dirs=[mercuryapi + 'pthreads-win32/x86']
+                  )],
+      # FIXME: update to include the path to pthreadVC2.dll
+      data_files = [('', [mercuryapi + 'pthreads-win32/x86/pthreadVC2.dll'])])
---
+++ python-mercuryapi-master/setup.py
@@ -0,0 +1,31 @@
+# python3 setup.py build
+# python3 setup.py sdist upload
+from setuptools import setup, Extension
+from distutils.command.build import build
+import os, platform
+
+class my_build(build):
+    def run(self):
+        if platform.system() == 'Darwin':
+            os.system("cp -f mercuryapi_osx.patch mercuryapi.patch")
+        os.system("make mercuryapi")
+        build.run(self)
+
+setup(name="python-mercuryapi", version="0.5.4",
+      author="Petr Gotthard",
+      author_email="petr.gotthard@centrum.cz",
+      description="Python wrapper for the ThingMagic Mercury API",
+      long_description=open('long_description.txt').read(),
+      url="https://github.com/gotthardp/python-mercuryapi",
+      classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta"
+      ],
+      cmdclass={'build': my_build},
+      ext_modules=[Extension("mercury",
+                             sources=["mercury.c"],
+                             libraries=["mercuryapi", "ltkc", "ltkctm"],
+                             include_dirs=['build/mercuryapi/include'],
+                             library_dirs=['build/mercuryapi/lib'])])
